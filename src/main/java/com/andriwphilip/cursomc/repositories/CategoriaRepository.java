e0a0e561d8d26760dc080991735eb9e71a7f913e
AndriwPhilip <andriwphilip@gmail.com>
2022-04-04 15:46:27 -0300

    Repository and Service to Category

---
+++ src/main/java/com/andriwphilip/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.andriwphilip.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.andriwphilip.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
